d3385eb4438b9ebf1f10815fe97357d097eb4102
julies01 <julie.bened@utbm.fr>
2024-12-28 10:26:55 +0100

    ajouts des types

--- neuron.h
+++ neuron.h
@@ -1,11 +1,19 @@
 #ifndef NEURON_H
 #define NEURON_H
 
+typedef struct Weight {
+    int value;
+    struct Weight *next;
+}  Weight ;
+
+typedef struct Weight *Weight_List;
+
 typedef struct Neuron {
-    double *weights;
-    double bias;
-    double output;
-    double error;
+   Weight_List weights;
+   int bias;
+   struct Neuron *next;
 } Neuron;
+
+typedef struct Neuron *Layer;
 
 #endif
